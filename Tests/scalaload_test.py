a35de6f997f0c9b4afd5c4ba8505f3a1cc6b5633
Wenslous Egesa <wayneotemahegesa@gmail.com>
2023-03-23 13:17:55 +0300

    php loadtest file

--- Tests/scalaload_test.py
+++ Tests/scalaload_test.py
@@ -2,8 +2,7 @@ from locust import HttpUser, task, between
 import random
 import decimal
 
-
-class MyPhpUser(HttpUser):
+class MyScalaUser(HttpUser):
     wait_time = between(0.5, 3)  # add some delay between requests
 
     @task
@@ -12,7 +11,8 @@ class MyPhpUser(HttpUser):
         data = {
                 'user': 'user{}'.format(random.randint(1, 1000)),
                 'currency': 'KSH',
-                'balance': random.randint(0, 10000),
-                'password': "password"
+                'balance': random.randint(0, 10000)
                 }
-        self.client.post("/user.php", json=data, headers=headers)
+        self.client.post("bank/", json=data, headers=headers)
+
+# locust -f my_test.py --headless --users 1000 --spawn-rate 1000 --host=http://127.0.0.1:8081 run command
